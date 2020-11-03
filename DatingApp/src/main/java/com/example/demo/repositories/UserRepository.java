27c076a44cfd4580bc53e07d415b448a0e754ecb
Rasmus Møller <rasm937k@stud.kea.dk>
2020-11-03 15:20:43 +0100

    changed db login credentials

--- DatingApp/src/main/java/com/example/demo/repositories/UserRepository.java
+++ DatingApp/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class UserRepository {
     private Connection establishConnection() throws SQLException {
         //Lav en forbindelse
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_company","dean","securePassword");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_company","user","password");
 
         return conn;
     }
