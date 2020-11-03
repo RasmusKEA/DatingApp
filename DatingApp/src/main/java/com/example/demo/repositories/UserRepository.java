db53342f20111707f94a49ceb9dbf9556534422f
Rasmus Møller <rasm937k@stud.kea.dk>
2020-11-03 15:18:53 +0100

    folders and establishConnection

---
+++ DatingApp/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repositories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class UserRepository {
+    private Connection establishConnection() throws SQLException {
+        //Lav en forbindelse
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_company","dean","securePassword");
+
+        return conn;
+    }
+}
