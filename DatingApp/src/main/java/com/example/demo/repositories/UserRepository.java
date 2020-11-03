5c9e7ff54d33267289347e0a94453b2cb3749e96
Rasmus Trap <rasm510f@stud.kea.dk>
2020-11-03 18:00:57 +0100

    bs virker ik

--- DatingApp/src/main/java/com/example/demo/repositories/UserRepository.java
+++ DatingApp/src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,18 +1,38 @@
 package com.example.demo.repositories;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.example.demo.models.User;
+
+import java.sql.*;
 
 public class UserRepository {
     private Connection establishConnection() throws SQLException {
         //Lav en forbindelse
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_company","user","password");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/dating?useLegacyDatetimeCode=false","user","password");
 
         return conn;
     }
 
-    private void createUser(){
+    public void createUser(String name, String email, String username, String password){
+        try {
+            PreparedStatement ps = establishConnection().prepareStatement("INSERT INTO users (username, password, fullname, email) VALUES (?, ?, ?, ?)");
 
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ps.setString(3, name);
+            ps.setString(4, email);
+
+            System.out.println(name);
+
+            ps.executeQuery();
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+
+            }
+
+        } catch (SQLException e) {
+        e.getMessage();
+
+        }
     }
 }
